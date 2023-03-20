2a322505cf20acee5393de8e96f55d8ee73ee0be
Ramzi Bouzaiene <ramzi.bouzaiene@sastec-group.com>
2023-03-20 16:19:35 +0100

    setup routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
